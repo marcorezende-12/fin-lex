9236e15e87eea3a4281930a19947008827e01794
Marco Aurelio <marco.aurelio.rsilva7@gmail.com>
2026-01-07 17:02:29 -0300

    fix(chart and transactions): return removed from code

--- app/charts/page.tsx
+++ app/charts/page.tsx
@@ -6,7 +6,7 @@ import Navbar from "../_components/navbar";
 const ChartsPage = async () => {
   const { userId } = await auth();
   if (!userId) {
-    return redirect("/login");
+    redirect("/login");
   }
   return (
     <>
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -6,7 +6,7 @@ import Navbar from "../_components/navbar";
 const TransactionsPage = async () => {
   const { userId } = await auth();
   if (!userId) {
-    return redirect("/login");
+    redirect("/login");
   }
   return (
     <>
